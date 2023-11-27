493624527e9c74725f0032261f2d3f5f7ef8c98e
Piitsi4 <146746525+Piitsi4@users.noreply.github.com>
2023-11-27 16:57:47 +0200

    Lisatty intro tarina pelin alkuun

---
+++ Assets/Scripts/OpeningStory/OpeningStory.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpeningStory : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        // Only specifing the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+    }
+}
