3c5c4d0e2962e9feb98905c360984c510a9fb149
Suamppa <18483942+Suamppa@users.noreply.github.com>
2024-03-01 02:27:03 +0200

    Add animator null check back

--- Assets/Scripts/Characters/Stats.cs
+++ Assets/Scripts/Characters/Stats.cs
@@ -173,10 +173,10 @@ public class Stats : MonoBehaviour
     {
         int effectiveDamage = incomingDamage - Defense;
         CurrentHealth -= effectiveDamage;
-        //if (animator != null)
-        //{
+        if (animator != null)
+        {
             animator.SetTrigger("Wounding");
-        //}
+        }
 
         if (Debug.isDebugBuild)
         {
